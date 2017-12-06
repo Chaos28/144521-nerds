a3942718e3629749789339c9c3ca13aeaebee289
Дмитрий <huntergeass@gmail.com>
2017-12-06 19:48:18 +0300

    правки JS

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
-var link=document.querySelector(".modal-open"),popup=document.querySelector(".modal"),close=popup.querySelector(".modal-close"),form=popup.querySelector("form"),user=popup.querySelector("[name=user-name]"),email=popup.querySelector("[name=email]"),storage=localStorage.getItem("user"),form=popup.querySelector("form");link.addEventListener("click",function(e){e.preventDefault(),popup.classList.add("modal-show"),storage?(user.value=storage,email.focus()):user.focus()}),close.addEventListener("click",function(e){e.preventDefault(),popup.classList.remove("modal-show"),popup.classList.remove("modal-error")}),form.addEventListener("submit",function(e){user.value&&email.value?localStorage.setItem("user-name",user.value):(e.preventDefault(),popup.classList.add("modal-error"))}),window.addEventListener("keydown",function(e){27===e.keyCode&&popup.classList.contains("modal-show")&&(popup.classList.remove("modal-show"),popup.classList.remove("modal-error"))});
+var link = document.querySelector(".modal-open");
+
+      var popup = document.querySelector(".modal");
+      var close = popup.querySelector(".modal-close");
+
+        var form = popup.querySelector("form");
+      var user = popup.querySelector("[name=user-name]");
+      var email = popup.querySelector("[name=email]");
+
+      var storage = localStorage.getItem("user");
+
+      var form = popup.querySelector("form");
+
+      link.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("modal-show");
+
+      if (storage) {
+      user.value = storage;
+      email.focus();
+    } else {
+      user.focus();
+    }
+
+});
+
+      close.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+});
+      
+    form.addEventListener("submit", function (evt) {
+      if (!user.value || !email.value) {
+        evt.preventDefault();
+
+        popup.classList.add("modal-error");
+      } else {
+      localStorage.setItem("user-name", user.value);
+    }
+});
+
+    window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
