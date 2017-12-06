f7a1f281403b0d54b53397d862dcd5eef060f9ea
Дмитрий <huntergeass@gmail.com>
2017-12-06 19:56:19 +0300

    Правка JS

--- js/script.js
+++ js/script.js
@@ -1,21 +1,50 @@
 var link = document.querySelector(".modal-open");
 
-    	var popup = document.querySelector(".modal");
-    	var close = popup.querySelector(".modal-close");
+      var popup = document.querySelector(".modal");
+      var close = popup.querySelector(".modal-close");
 
-    	 var name = popup.querySelector("[name=name]");
-  		var email = popup.querySelector("[name=email]");
+        var form = popup.querySelector("form");
+      var user = popup.querySelector("[name=user-name]");
+      var email = popup.querySelector("[name=email]");
 
-    	var form = popup.querySelector("form");
+      var storage = localStorage.getItem("user");
 
-    	link.addEventListener("click", function (evt) {
-    	evt.preventDefault();
-    	popup.classList.add("modal-show");
+      var form = popup.querySelector("form");
 
+      link.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("modal-show");
 
-  	});
+      if (storage) {
+      user.value = storage;
+      email.focus();
+    } else {
+      user.focus();
+    }
 
-	    close.addEventListener("click", function (evt) {
-	    evt.preventDefault();
-	    popup.classList.remove("modal-show");
-  	});
+});
+
+      close.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+});
+      
+    form.addEventListener("submit", function (evt) {
+      if (!user.value || !email.value) {
+        evt.preventDefault();
+
+        popup.classList.add("modal-error");
+      } else {
+      localStorage.setItem("user-name", user.value);
+    }
+});
+
+    window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
